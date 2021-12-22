ddd375d0b2e7206436b8a31c94cdd7c4bfd948dd
Dreamer <temirkhan.onyx@gmail.com>
2021-12-22 15:50:46 +0300

    Initial commit

--- src/header/show/show.js
+++ src/header/show/show.js
@@ -1,5 +1,4 @@
-import { useParams } from 'react-router';
-import { Link } from 'react-router-dom';
+import { Link,useParams } from 'react-router-dom';
 import GetHistoryLocation from '../../locate';
 import { getFirestore,collection, addDoc,doc, getDoc } from "firebase/firestore";
 import  '../../data';
