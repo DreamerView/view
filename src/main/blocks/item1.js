56161d335fa2f1cca08ff51737f4c8ac93c64732
Dreamer <temirkhan.onyx@gmail.com>
2022-01-15 20:22:15 +0300

    h

--- src/main/blocks/item1.js
+++ src/main/blocks/item1.js
@@ -1,6 +1,5 @@
 import {memo} from 'react';
 import { Link } from "react-router-dom";
-import GetHistoryLocation from "../../locate";
 
 const Item1 = (products)=> {
     const ScrollToTop = () => {
@@ -10,7 +9,7 @@ const Item1 = (products)=> {
         <div className="m-d-b">
               <Link onClick={ScrollToTop} className="m-p-b" to={`/products/${products.item.id}`}>
                 <div className="m-i-1-1">
-                  <img loading="lazy" src={products.item.image?GetHistoryLocation + products.item.image:GetHistoryLocation+"/images/first-aid-alt.svg"} alt="Panakea 1" className="m-i" />
+                  <img loading="lazy" src={products.item.image?process.env.PUBLIC_URL + products.item.image:process.env.PUBLIC_URL+"/images/first-aid-alt.svg"} alt="Panakea 1" className="m-i" />
                 </div>
                 <div className="m-t-p">
                   {products.item.title?<h1 className="m-t">{products.item.title}</h1>:<><div className="skeleton m-t-loader"/><div className="skeleton m-t-1-loader"/></>}
@@ -20,7 +19,7 @@ const Item1 = (products)=> {
                       {products.item.price?<h1 className="l-p">{products.item.price} ₸</h1>:<div className="skeleton l-p-loader"/>}
                       <div className="p-p">
                         <div className="i-p-1">
-                          <img loading="lazy" src={GetHistoryLocation+"/images/shopping-basket-add.svg"} alt="Panakea 1" className="p" />
+                          <img loading="lazy" src={process.env.PUBLIC_URL+"/images/shopping-basket-add.svg"} alt="Panakea 1" className="p" />
                         </div>
                       </div>
                     </div>
