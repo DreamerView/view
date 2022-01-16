06fbcb2fbd1ea2b688cd1fd8b172edb590caa806
Dreamer <temirkhan.onyx@gmail.com>
2022-01-16 23:59:06 +0600

    g

--- src/header/panel/panel.js
+++ src/header/panel/panel.js
@@ -1,10 +1,22 @@
+import { useState,useEffect } from "react";
 import PanelList from "./panel-list";
 
 const Panel = () => {
+    const [city,setCity] = useState('Loading');
+    useEffect(()=>{
+        fetch('http://ip-api.com/json')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (payload) {
+            setCity(payload.city);
+        });
+    },[]);
+
     return(
         <div className="m-p-i-b">
             <PanelList item={{images:process.env.PUBLIC_URL+'/images/info.svg',title:'Гид'}} />
-            <PanelList item={{images:process.env.PUBLIC_URL+'/images/compass.svg',title:'Караганда'}} />
+            <PanelList item={{images:process.env.PUBLIC_URL+'/images/compass.svg',title:city}} />
             <PanelList item={{images:process.env.PUBLIC_URL+'/images/earth.svg',title:'Русский'}} />
         </div>
     );
--- src/header/search/search.jsx
+++ src/header/search/search.jsx
@@ -10,6 +10,7 @@ const Search = () => {
     const focus = useRef();
     const [check,setCheck] = useState('');
     const [send,setSend] = useState([{}]);
+    const [block,setBlock] = useState('none');
     const [inputChange,setInputChange] = useState('');
     const [lazyblock,setLazyBlock] = useState('');
     const search = useSelector(state => state.search);
@@ -20,12 +21,14 @@ const Search = () => {
             setCheck(true);
             dispatch({type:'SetHeader',set:false});
             document.getElementsByTagName('html')[0].style.cssText= "overflow-y:hidden;";
+            setBlock('flex')
         };
         const CheckRemove = ()=> {
             setTimeout(()=>{
                 setCheck(false);
                 dispatch({type:'SetHeader',set:true});
                 document.getElementsByTagName('html')[0].style.cssText= "";
+                setBlock('none');
             },250);
         };
         focus.current.addEventListener('focus',CheckEvent);
@@ -70,7 +73,13 @@ const Search = () => {
         <>
         {check?<div className="m-s-fixed"/>:''}
         <div className="m-s">
-            <input ref={focus} spellCheck="false" type="text" placeholder="Какой препарат мы будем искать?" onChange={(e)=>{setInputChange(e.target.value)}} className="m-s-1" />
+            <div className="m-s-flex">
+                <input ref={focus} spellCheck="false" type="text" placeholder="Какой препарат мы будем искать?" onChange={(e)=>{setInputChange(e.target.value)}} className="m-s-1" />
+                <div style={{display:block}} className="m-s-flex-close">
+                    Закрыть
+                    {/* <img src={process.env.PUBLIC_URL+"/images/close.svg"} alt="" className="m-s-flex-close-i" /> */}
+                    </div>
+            </div>
         </div>
         {check?lazyblock?<SearchResult item={send} result={inputChange} key={new Date()} />:<SearchLoader/>:<div/>}
         </>
