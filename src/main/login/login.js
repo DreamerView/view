19a213e73ecc624cf662e6f599135bf56a779035
Dreamer <temirkhan.onyx@gmail.com>
2022-01-18 18:46:14 +0600

    Login Form

--- src/main/login/login.js
+++ src/main/login/login.js
@@ -1,74 +1,54 @@
 const LoginForm = () => {
     return(
         <div className="m">
-  <div className="join-text-place">
-    <h1 className="m-p">Войти с помощью</h1>
-    <h1 className="m-p-content">У вас нет аккаунта? <button className="regis-green">Зарегистрироваться</button></h1>
-  </div>
-  <div className="main-four-blocks-place">
-      <div className="place-three-blocks">
-        <div className="first-block-in">
-          <h1 className="electron-addres">Электронный адрес</h1>
-          <div className="place-input-in">
-            <input type="email" placeholder="name@example.com" className="none-border" />
-          </div>
-          <div>
-            <h1 className="electron-addres">Пароль</h1>
-            <div className="place-input-in">
-              <input type="password" placeholder="********************" className="none-border" />
-              
+          <div className="m-block">
+            <h1 className="m-p">Добро пожаловать</h1>
+            <h1 className="m-p-content">У вас нет аккаунта? <a className="m-p-content-url" href="s">Зарегистрируйтесь</a></h1>
+            <div className="login-form">
+              <div className="login-form-content">
+                <div className="login-form-content-part">
+                  <input type="text" placeholder="Телефон или email" className="login-form-content-input" />
+                  <input type="text" placeholder="Пароль" className="login-form-content-input" />
+                  <button type="button" className="login-form-content-button">Войти</button>
+                  <a href="s" className="login-form-content-forget">Забыли пароль?</a>
+                </div>
+              </div>
+              <div className="login-form-select">
+                <div className="login-form-select-border"/>
+                <h1 className="login-form-select-text">или</h1>
+                <div className="login-form-select-border"/>
+              </div>
+              <div className="login-form-content">
+                <div className="login-form-content-part-1">
+                  <button type="button" className="login-form-content-sn">
+                    <div className="login-form-content-sn-content">
+                    <div className="login-form-content-sn-picture">
+                      <img src={process.env.PUBLIC_URL+"/login-images/Google__G__Logo.svg.png"} className="login-form-content-sn-image" alt="social"/>
+                    </div>
+                    <span className="login-form-content-sn-text">Войти через Google</span>
+                    </div>
+                  </button>
+                  <button type="button" className="login-form-content-sn">
+                    <div className="login-form-content-sn-content">
+                    <div className="login-form-content-sn-picture">
+                      <img src={process.env.PUBLIC_URL+"/login-images/facebook_logo_icon_147291.png"} className="login-form-content-sn-image" alt="social"/>
+                    </div>
+                    <span className="login-form-content-sn-text">Войти через Facebook</span>
+                    </div>
+                  </button>
+                  <button type="button" className="login-form-content-sn">
+                    <div className="login-form-content-sn-content">
+                    <div className="login-form-content-sn-picture">
+                      <img src={process.env.PUBLIC_URL+"/login-images/Apple_logo_black.svg.png"} className="login-form-content-sn-image" alt="social"/>
+                    </div>
+                    <span className="login-form-content-sn-text">Войти через Apple</span>
+                    </div>
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="place-button-in">
-            <button className="button-join-in">
-              ВОЙТИ
-            </button>
-          </div>
         </div>
-        <div className="second-block-in">
-          <div className="vertical-stick" />
-          <div className="place-or">
-            <h1 className="or">ИЛИ</h1>
-          </div>
-          <div className="vertical-stick" />
-        </div>
-        <div className="third-block-in">
-          <div className="place-google-apple-facebook">
-            <button className="google">
-              <div className="place-img-in-button">
-                <img className="img-in-button" src={`${process.env.PUBLIC_URL}/login-images/Google__G__Logo.svg.png`} alt="Google" /> 
-              </div>
-              Войти через Google
-            </button>
-          </div>
-          <div className="place-google-apple-facebook">
-            <button className="google">
-              <div className="place-img-in-button">
-                <img className="img-in-button" src={`${process.env.PUBLIC_URL}/login-images/Apple_logo_black.svg.png`} alt="Apple" /> 
-              </div>
-              Войти через Apple
-            </button>
-          </div>
-          <div className="place-google-apple-facebook">
-            <button className="google">
-              <div className="place-img-in-button">
-                <img className="img-in-button" src={`${process.env.PUBLIC_URL}/login-images/facebook_logo_icon_147291.png`} alt="Facebook" /> 
-              </div>
-              Войти через Facebook
-            </button>
-          </div>
-        </div>
-        <div className="four-block-in">
-          <div className="place-registration">
-            <div className="place-secure-captcha">
-              <h1 className="secure-captcha">Secure Login with reCAPTCHA subject to Google</h1>
-              <h1 className="secure-privacy">Terms &amp; privacy</h1>
-            </div>
-          </div>       
-        </div>
-      </div>
-  </div>
-</div>
     );
 };
 
